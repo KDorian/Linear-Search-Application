d3d2ab33174501639f282838135fe24bc23736fd
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-03 03:37:21 +0200

    Add Action Listener to buttons

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,18 @@
  * Created by Brodull on 03.09.2017.
  */
 public class Main {
+
+    public static void main(String[] args){
+
+        NewJFrame Fr = new NewJFrame();
+
+        Fr.setLocationRelativeTo(null);
+
+        Fr.setVisible(true);
+
+        Fr.setSize(405,320);
+        Fr.setResizable(false);
+
+
+    }
 }
---
+++ src/NewJFrame.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Brodull on 03.09.2017.
+ */
+public class NewJFrame extends JFrame {
+
+    Search obj = new Search();
+
+    private JTextField size;
+    private JButton setSizeButton;
+    private JTextField num;
+    private JButton linearSearchButton;
+    private JLabel SetArray;
+    private JTextArea Print;
+    private JPanel panel;
+
+
+    public NewJFrame() {
+        setContentPane(panel);
+        setSizeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int n;
+
+                String st = size.getText();
+                n = Integer.parseInt(st);
+
+                obj.initializeArray(n);
+                SetArray.setText(obj.printArray());
+            }
+        });
+        linearSearchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int n;
+
+                String st = num.getText();
+                n = Integer.parseInt(st);
+
+                String a = obj.serialSearch(n);
+                Print.setText(a);
+
+            }
+        });
+    }
+}
